4f92500e0395e0c90dd137d76b01bd648038299b
itumeleng-itu <matomempaola@gmail.com>
2025-06-17 12:03:02 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type React from "react"
 import "@/app/globals.css"
 import { Inter } from "next/font/google"
 import { Toaster } from "@/components/ui/toaster"
-
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
